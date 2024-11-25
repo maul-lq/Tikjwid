72df3a2b0071d8c8b8395d8b3e16d2bbd0e8a1b1
Rizlrad Fz <rizlradfz@gmail.com>
2024-11-25 09:19:30 +0700

    test

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>TIK Tajwid - Kumpulan Tajwid Al-Quran</title>
+</head>
+<body class=" font-sans overflow-hidden">
+    <nav class="flex bg-slate-500 text-white px-8 py-4 justify-between">
+        <a href="home.html">Logo</a>
+        <div class="flex">
+            <a class="px-2" href="#">Sukun dan Tanwin</a>
+            <a class="px-2" href="#">Mim Sukun</a>
+            <a class="px-2" href="#">Lam Ta'rief</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+            <a class="px-2" href="#">Lorem Ipsum</a>
+        </div>
+    </nav>
+    <div class="mt-40 px-16 flex justify-between">
+        <div>
+            <div class="text-4xl font-extrabold">NAMA WEB</div>
+            <div class="w-1/2 text-justify mt-6">tajwid adalah membaguskan bacaan huruf-huruf atau kalimat-kalimat Al-Qur'an satu persatu dengan terang, teratur, perlahan, dan tidak tergesa-gesa. Sehingga, ilmu tajwid dapat diartikan sebagai ilmu yang mempelajari cara membaca Al-Qur'an dengan baik dan benar. </div>
+        </div>
+        <div>logo</div>
+    </div>
+</body>
+</html>
