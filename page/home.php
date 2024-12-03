9342644ff4ec648c4bad988c938c8d7a934e26e5
Rizlrad Fz <rizlradfz@gmail.com>
2024-12-03 15:43:49 +0700

    Update v03440503122024

---
+++ page/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+include './../superpage.php';
+Head();
+?>
+
+<body class="font-sans">
+    <?php NavBar(); ?>
+    <div class="mt-40 px-16 flex justify-between">
+        <div>
+            <div class="text-4xl font-extrabold">NAMA WEB</div>
+            <div class="w-1/2 text-justify mt-6">tajwid adalah membaguskan bacaan huruf-huruf atau kalimat-kalimat Al-Qur'an satu persatu dengan terang, teratur, perlahan, dan tidak tergesa-gesa. Sehingga, ilmu tajwid dapat diartikan sebagai ilmu yang mempelajari cara membaca Al-Qur'an dengan baik dan benar. </div>
+        </div>
+        <div>logo</div>
+    </div>
+</body>
+
+<?php Scripts(); ?>
+
+</html>
