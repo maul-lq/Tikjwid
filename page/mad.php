bd72805412d830d8805e3362ec757b1028cd8e04
Rizlrad Fz <rizlradfz@gmail.com>
2024-12-07 10:58:42 +0700

    fix 1867b20: Update v10404007122024 …

--- page/mad.php
+++ page/mad.php
@@ -7,18 +7,20 @@
     ?>
 </head>
 
-<body class="font-sans  <?=  $WEBMODE ?>">
-<?php NavBar(7); ?>
-<main class="bg-white dark:bg-gray my-3 flex items-center min-h-screen rounded-lg max-w-screen m-10">
+<body class="font-sans  <?= $WEBMODE ?>">
+    <?php NavBar(8); ?>
+    <main class="bg-white dark:bg-gray my-3 flex items-center min-h-screen rounded-lg max-w-screen m-10">
         <div class="bg-white dark:bg-gray-900 dark:text-white min-h-screen p-6  rounded-lg shadow-lg ">
-            <h1 class="text-xl text-gray-600 dark:text-white font-bold mb-4">Tikjwid</h1>
-            <p class="text-gray-600 dark:text-white text-justify">
-                Tikjwid adalah sebuah website yang membantu membaca Al-Qur'an secara online, terutama bagi orang-orang yang ingin belajar dan mencintai Al-Qur'an. Website ini memberikan contoh-contoh pernyataan, ayat-ayat, dan tafsir yang dapat membantu mereka memahami Al-Qur'an.
-            </p>
+            <h1 class="text-xl text-gray-600 dark:text-white font-bold mb-4">Nun Sukun</h1>
+            <p class="text-gray-600 dark:text-white text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!</p>
+            <br>
+            <h1 class="text-xl text-gray-600 dark:text-white font-bold mb-4">Tanwin</h1>
+            <p class="text-gray-600 dark:text-white text-justify">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur dolores eum at minus nisi vitae fugiat, corporis facilis veritatis. Libero officiis similique facere saepe nisi fugiat dolorum placeat nemo!</p>
         </div>
     </main>
 </body>
 
-<?php Scripts();Footer(false); ?>
+<?php Scripts();
+Footer(false); ?>
 
 </html>
--- superpage.php
+++ superpage.php
@@ -3,7 +3,7 @@
 
 <?php
 $HOSTURL = "http://127.0.0.1/tikjwid/";
-$WEBMODE = "";
+$WEBMODE = "white";
 
 function pindahHalamanKe($url)
 {
@@ -74,20 +74,20 @@ function pindahHalamanKe($url)
                         <a href="<?= $HOSTURL; ?>page/lamtebaldantipis.php" <?= ($pageNumber == 6) ? $active : $nonactive; ?>>Lam Tebal & Lam Tipis</a>
                     </li>
                     <li>
-                        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="<?= ($pageNumber == 7) ? 'flex items-center justify-between w-full py-2 px-3 text-blue-700 rounded hover:bg-blue-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-blue-500 md:dark:hover:text-blue-500 dark:focus:text-blue-500 dark:border-blue-500 dark:hover:bg-blue-500 md:dark:hover:bg-transparent' : 'flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent' ?>">Idgham <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                        <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="<?= ($pageNumber == 7) ? 'flex items-center justify-between w-full py-2 px-3 text-blue-700 rounded hover:bg-blue-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-blue-500 md:dark:hover:text-blue-500 dark:focus:text-blue-500 dark:border-blue-500 dark:hover:bg-blue-500 md:dark:hover:bg-transparent' : 'flex items-center justify-between w-full py-2 px-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:p-0 md:w-auto dark:text-gray-500 md:dark:hover:text-gray-500 dark:focus:text-gray-500 dark:border-gray-500 dark:hover:bg-gray-500 md:dark:hover:bg-transparent' ?>">Idgham <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
                             </svg></button>
                         <!-- Dropdown menu -->
-                        <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
-                            <ul class="py-2 text-sm text-white-700 dark:text-gray-100" aria-labelledby="dropdownLargeButton">
+                        <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-white rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                            <ul class="py-2 text-sm text-gray-700 dark:text-gray-100" aria-labelledby="dropdownLargeButton">
                                 <li>
-                                    <a href="<?= $HOSTURL; ?>page/idgham.php#Mutamasilain" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mutamatsilain</a>
+                                    <a href="<?= $HOSTURL; ?>page/idgham.php#Mutamasilain" class="block px-4 py-2 dark:text-white text-gray-600 dark:hover:text-blue">Mutamatsilain</a>
                                 </li>
                                 <li>
-                                    <a href="<?= $HOSTURL; ?>page/idgham.php#Mutaqaribain" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mutaqaribain</a>
+                                    <a href="<?= $HOSTURL; ?>page/idgham.php#Mutaqaribain" class="block px-4 py-2 dark:text-white text-gray-600 dark:hover:text-blue">Mutaqaribain</a>
                                 </li>
                                 <li>
-                                    <a href="<?= $HOSTURL; ?>page/idgham.php#Mutajanisain" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mutajanisain</a>
+                                    <a href="<?= $HOSTURL; ?>page/idgham.php#Mutajanisain" class="block px-4 py-2 dark:text-white text-gray-600 dark:hover:text-blue">Mutajanisain</a>
                                 </li>
                             </ul>
                         </div>
