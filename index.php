1867b20c18af59d699ec3fc9e56dcca8b62a9c39
Rizlrad Fz <rizlradfz@gmail.com>
2024-12-07 10:40:31 +0700

    Update v10404007122024 Changes to be committed: 	modified:   index.php 	deleted:    package-lock.json 	deleted:    package.json

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ include_once "./superpage.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loading</title>
-    <meta http-equiv="refres" content="5; url=<?=$HOSTURL?>page/home.php">
+    <meta http-equiv="refresh" content="3; url=<?=$HOSTURL?>page/home.php">
 </head>
 <body class="font-sans  <?=  $WEBMODE ?>">
     
