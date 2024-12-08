b58d854e9834044e1edff76a3fe2361a55ac1506
Rizlrad Fz <rizlradfz@gmail.com>
2024-12-09 00:12:21 +0700

    1234

---
+++ Tikjwid-main/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+include_once "./superpage.php";
+?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Loading</title>
+    <meta http-equiv="refresh" content="1; url=<?=$HOSTURL?>page/home.php">
+</head>
+<body class="font-sans dark">
+    
+</body>
+</html>
---
+++ Tikjwid-main/page/mimsukun.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once "./../superpage.php";
+    Head();
+    ?>
+</head>
+
+<body class="font-sans dark">
+    <?php NavBar(3); ?>
+
+    <main class="bg-white dark:bg-gray my-3 flex items-center min-h-screen max-w-screen m-10">
+        <div class="bg-white dark:bg-gray-900 dark:text-white min-h-screen p-6 rounded-lg shadow-lg ">
+            <h1 class="text-xl text-gray-600 dark:text-white font-bold mb-4">Mim Sukun</h1>
+            <p class="text-gray-600 dark:text-white text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse culpa aspernatur quas debitis beatae vitae est nostrum. Minus quidem quasi ducimus voluptas praesentium sequi est tempore adipisci. Exercitationem, tempore libero.Tikjwid adalah sebuah website yang membantu membaca Al-Qur'an secara online, terutama bagi orang-orang yang ingin belajar dan mencintai Al-Qur'an. Website ini memberikan contoh-contoh pernyataan, ayat-ayat, dan tafsir yang dapat membantu mereka memahami Al-Qur'an.
+            </p>
+        </div>
+    </main>
+
+</body>
+
+<?php Scripts();
+Footer(false); ?>
+
+</html>
